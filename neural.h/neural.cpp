e0023bf9ecc2c6546a58263535236981b47123aa
subeenregmi <subeenregmi123@gmail.com>
2023-07-24 17:28:05 +0100

    implemente matricies

--- neural.h/neural.cpp
+++ neural.h/neural.cpp
@@ -1,6 +1,16 @@
 #define NEURAL_IMPLEMENTATIONS
 #include "neural.h"
 
+using namespace std;
+
 int main(){
-	return 0;
+	Matrix M;
+	M.rows = 10;
+	M.cols = 10;
+	MATRIX_MEMORY_ALLOCATE(&M);
+	MATRIX_SET_ITEM_AT(&M, 1.0f, 0, 0);
+	MATRIX_SET_ITEM_AT(&M, 1.0f, 1, 1);
+	MATRIX_PRINT(&M);
+
+	MATRIX_DESTROY(&M);
 }
--- neural.h/neural.h
+++ neural.h/neural.h
@@ -2,33 +2,79 @@
 #define NEURAL_H
 
 #include <iostream>
+#include <cassert>
 
 typedef float dataT;
 
 struct Matrix{
-	int rows;
-	int cols;
-	*dataT start;
-}
+	int rows = 0;
+	int cols = 0;
+	dataT* start = nullptr;
+};
 
 // Init functions
-
-void MATRIX_MEMORY_ALLOCATE();
-void MATRIX_DESTROY();
+void MATRIX_MEMORY_ALLOCATE(Matrix* M);
+void MATRIX_DESTROY(Matrix* M);
+void MATRIX_IDENTITY(Matrix* M);
 
 // Mathematical functions
 void MATRIX_DOT(Matrix D, Matrix A, Matrix B);
 void MATRIX_ADD(Matrix D, Matrix A, Matrix B);
 
 // Miscellaneous functions
-void MATRIX_PRINT();
-bool MATRIX_SET_ITEM_AT(Matrix M, dataT item);
-dataT MATRIX_GET_ITEM_AT(Matrix M, int rows, int cols);
+void MATRIX_PRINT(Matrix* M);
+dataT MATRIX_GET_ITEM_AT(Matrix* M, int rows, int cols);
+void MATRIX_SET_ITEM_AT(Matrix* M, dataT item, int rows, int cols);
 
 #endif // NEURAL_H
 
 #ifdef NEURAL_IMPLEMENTATIONS
 #define NEURAL_IMPLEMENTATIONS
 
+// Init functions
+void MATRIX_MEMORY_ALLOCATE(Matrix* M){
+	assert(M->rows != 0);	
+	assert(M->cols != 0);
+	assert(M->start == nullptr && "Matrix has already been allocated, destroy/free memory first");
+
+	M->start = (dataT*)malloc(sizeof(dataT)*M->rows*M->cols);
+	assert(M->start != nullptr);
+}
+
+void MATRIX_DESTROY(Matrix* M){
+	assert(M->start != nullptr && "Matrix has not been allocated.");
+	free(M->start);
+	M->start = nullptr;
+}
+
+void MATRIX_IDENTITY(Matrix* M){
+	assert(M->rows != 0);
+	assert(M->cols != 0);
+	assert(M->start != nullptr);
+
+	// Implement
+}
+
+// Miscellaneous functions 
+void MATRIX_PRINT(Matrix* M){
+	for(int i=0; i<M->rows; i++){
+		for(int j=0; j<M->cols; j++){
+			printf("%f	", MATRIX_GET_ITEM_AT(M, i, j));
+		}
+		printf("\n\n");
+	}
+}
+
+dataT MATRIX_GET_ITEM_AT(Matrix* M, int rows, int cols){
+	return M->start[rows*M->cols + cols];
+}
+
+void MATRIX_SET_ITEM_AT(Matrix* M, dataT item, int rows, int cols){
+	assert(M->start != nullptr);
+	assert(M->rows > rows);
+	assert(M->cols > cols);
+	
+	M->start[rows*M->cols + cols] = item;
+}
 
 #endif // NEURAL_IMPLEMENTATIONS
