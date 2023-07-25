2f2eba2db0c2ba78b426ef5032722c888e60a4e5
subeenregmi <subeenregmi123@gmail.com>
2023-07-25 03:23:57 +0100

    implemented namespaces and randomizing matricies

--- neural.h/neural.cpp
+++ neural.h/neural.cpp
@@ -2,36 +2,17 @@
 #include "neural.h"
 
 using namespace std;
+using namespace NMatrix;
 
 int main(){
-	Matrix M = {.rows = 2, .cols = 2};
-	MATRIX_MEMORY_ALLOCATE(&M);
+	srand(time(0));
 
-	MATRIX_SET_ITEM_AT(&M, 1.0f, 0, 0);
-	MATRIX_SET_ITEM_AT(&M, 2.0f, 0, 1);
-	MATRIX_SET_ITEM_AT(&M, 3.0f, 1, 0);
-	MATRIX_SET_ITEM_AT(&M, 4.0f, 1, 1);
-	
-	Matrix D; 
-	D.rows = 2;
-	D.cols = 2;
-	MATRIX_MEMORY_ALLOCATE(&D);
+	Matrix M = {.rows = 5, .cols = 5};
 
-	MATRIX_SET_ITEM_AT(&D, 5.0f, 0, 0);
-	MATRIX_SET_ITEM_AT(&D, 3.5f, 0, 1);
-	MATRIX_SET_ITEM_AT(&D, 6.9f, 1, 0);
-	MATRIX_SET_ITEM_AT(&D, 0.03f, 1, 1);
+	MEMORY_ALLOC(&M);
 
-	Matrix C;
-	C.rows = 2;
-	C.cols = 2;
-	MATRIX_MEMORY_ALLOCATE(&C);
-		
-	MATRIX_DOT(&C, &M, &D);	
+	RANDOMIZE(&M);
+	PRINT(&M);
 
-	MATRIX_PRINT(&C);
-
-	MATRIX_DESTROY(&M);
-	MATRIX_DESTROY(&D);
-	MATRIX_DESTROY(&C);
+	MEMORY_DEALLOC(&M);
 }
--- neural.h/neural.h
+++ neural.h/neural.h
@@ -3,122 +3,163 @@
 
 #include <iostream>
 #include <cassert>
+#include <cstdlib>
+#include <time.h>
+#include <math.h>
 
 typedef float dataT;
 
-struct Matrix{
-	int rows = 0;
-	int cols = 0;
-	dataT* start = nullptr;
-};
+// General functions
+dataT randD();
 
-// Init functions
-void MATRIX_MEMORY_ALLOCATE(Matrix* M);
-void MATRIX_DESTROY(Matrix* M);
-void MATRIX_IDENTITY(Matrix* M);
+// Loss Functions
+namespace LossF{
+	
+}
 
-// Mathematical functions
-void MATRIX_DOT(Matrix* D, Matrix* A, Matrix* B);
-void MATRIX_ADD(Matrix* D, Matrix* A, Matrix* B);
+// Matricies
+namespace NMatrix{
 
-// Miscellaneous functions
-void MATRIX_PRINT(Matrix* M);
-dataT MATRIX_GET_ITEM_AT(Matrix* M, int rows, int cols);
-void MATRIX_SET_ITEM_AT(Matrix* M, dataT item, int rows, int cols);
+	typedef float dataT;
+
+	struct Matrix{
+		int rows = 0;
+		int cols = 0;
+		dataT* start = nullptr;
+	};
+
+	// Init functions
+	void MEMORY_ALLOC(Matrix* M);
+	void MEMORY_DEALLOC(Matrix* M);
+	void IDENTITY(Matrix* M);
+	void RANDOMIZE(Matrix* M);
+
+	// Mathematical functions
+	void DOT(Matrix* D, Matrix* A, Matrix* B);
+	void ADD(Matrix* D, Matrix* A, Matrix* B);
+
+	// Miscellaneous functions
+	void PRINT(Matrix* M);
+	dataT GET_ITEM(Matrix* M, int rows, int cols);
+	void SET_ITEM(Matrix* M, dataT item, int rows, int cols);
+}
 
 #endif // NEURAL_H
 
 #ifdef NEURAL_IMPLEMENTATIONS
 #define NEURAL_IMPLEMENTATIONS
 
-// Init functions
-void MATRIX_MEMORY_ALLOCATE(Matrix* M){
-	assert(M->rows != 0);	
-	assert(M->cols != 0);
-	assert(M->start == nullptr && "Matrix has already been allocated, destroy/free memory first");
-
-	M->start = (dataT*)malloc(sizeof(dataT)*M->rows*M->cols);
-	assert(M->start != nullptr);
+dataT randD(){
+	return (dataT) rand() / RAND_MAX;
 }
 
-void MATRIX_DESTROY(Matrix* M){
-	assert(M->start != nullptr && "Matrix has not been allocated.");
-	free(M->start);
-	M->start = nullptr;
+// Loss Functions
+namespace LossF{
+
 }
 
-void MATRIX_IDENTITY(Matrix* M){
-	assert(M->rows != 0);
-	assert(M->cols != 0);
-	assert(M->start != nullptr);
-
-	for(int i=0; i<M->rows; i++){
-		for(int j=0; j<M->cols; j++){
-			if(i == j){
-				M->start[i*M->cols + j] = 1.0f;
-			}
-			else{
-				M->start[i*M->cols + j] = 0.0f;
-			}
-		}
+// Matricies
+namespace NMatrix{
+	// Init functions
+	void MEMORY_ALLOC(Matrix* M){
+		assert(M->rows != 0);	
+		assert(M->cols != 0);
+		assert(M->start == nullptr && "Matrix has already been allocated, destroy/free memory first");
+		M->start = (dataT*)malloc(sizeof(dataT)*M->rows*M->cols);
+		assert(M->start != nullptr);
 	}
-}
 
-// Mathematical functions
+	void MEMORY_DEALLOC(Matrix* M){
+		assert(M->start != nullptr && "Matrix has not been allocated.");
+		free(M->start);
+		M->start = nullptr;
+	}
 
-void MATRIX_DOT(Matrix* D, Matrix* A, Matrix* B){
-	// D = A * B
-	assert(A->cols == B->rows);	
-	assert(D->rows == A->rows);
-	assert(D->cols == B->cols);
+	void IDENTITY(Matrix* M){
+		assert(M->rows != 0);
+		assert(M->cols != 0);
+		assert(M->start != nullptr);
 
-	for(int i=0; i<A->rows; i++){
-		for(int j=0; j<B->cols; j++){
-			for(int k=0; k<A->cols; k++){
-				D->start[i*D->cols+j] += A->start[i*A->cols+k] * B->start[k*B->rows+j];
+		for(int i=0; i<M->rows; i++){
+			for(int j=0; j<M->cols; j++){
+				if(i == j){
+					M->start[i*M->cols + j] = 1.0f;
+				}
+				else{
+					M->start[i*M->cols + j] = 0.0f;
+				}
 			}
 		}
 	}
 
-}
+	void RANDOMIZE(Matrix* M){
+		assert(M->rows != 0);
+		assert(M->cols != 0);
+		assert(M->start != nullptr);
 
-void MATRIX_ADD(Matrix* D, Matrix* A, Matrix* B){
-	assert(A->rows == B->rows);
-	assert(A->cols == B->cols);
-	assert(D->rows == A->rows);
-	assert(D->cols == A->cols);
-	assert(A->start != nullptr && "Matrix A (2nd param) has not been allocated");
-	assert(B->start != nullptr && "Matrix B (3rd param) has not been allocated");
-	assert(D->start != nullptr && "Matrix D (1nd param) has not been allocated");
-	
-	for(int i=0; i<A->rows; i++){
-		for(int j=0; j<A->cols; j++){
-			D->start[i*A->cols + j] = A->start[i*A->cols + j] + B->start[i*A->cols + j];
+		for(int i=0; i<M->rows; i++){
+			for(int j=0; j<M->cols; j++){
+				M->start[i*M->cols + j] = randD();
+			}
 		}
 	}
 
-}
 
-// Miscellaneous functions 
-void MATRIX_PRINT(Matrix* M){
-	for(int i=0; i<M->rows; i++){
-		for(int j=0; j<M->cols; j++){
-			printf("%f	", MATRIX_GET_ITEM_AT(M, i, j));
+	// Mathematical functions
+
+	void DOT(Matrix* D, Matrix* A, Matrix* B){
+		// D = A * B
+		assert(A->cols == B->rows);	
+		assert(D->rows == A->rows);
+		assert(D->cols == B->cols);
+
+		for(int i=0; i<A->rows; i++){
+			for(int j=0; j<B->cols; j++){
+				for(int k=0; k<A->cols; k++){
+					D->start[i*D->cols+j] += A->start[i*A->cols+k] * B->start[k*B->rows+j];
+				}
+			}
 		}
-		printf("\n\n");
+	}
+
+	void ADD(Matrix* D, Matrix* A, Matrix* B){
+		assert(A->rows == B->rows);
+		assert(A->cols == B->cols);
+		assert(D->rows == A->rows);
+		assert(D->cols == A->cols);
+		assert(A->start != nullptr && "Matrix A (2nd param) has not been allocated");
+		assert(B->start != nullptr && "Matrix B (3rd param) has not been allocated");
+		assert(D->start != nullptr && "Matrix D (1nd param) has not been allocated");
+		
+		for(int i=0; i<A->rows; i++){
+			for(int j=0; j<A->cols; j++){
+				D->start[i*A->cols + j] = A->start[i*A->cols + j] + B->start[i*A->cols + j];
+			}
+		}
+
+	}
+
+
+	// Miscellaneous functions 
+	void PRINT(Matrix* M){
+		for(int i=0; i<M->rows; i++){
+			for(int j=0; j<M->cols; j++){
+				printf("%f	", GET_ITEM(M, i, j));
+			}
+			printf("\n\n");
+		}
+	}
+
+	dataT GET_ITEM(Matrix* M, int rows, int cols){
+		return M->start[rows*M->cols + cols];
+	}
+
+	void SET_ITEM(Matrix* M, dataT item, int rows, int cols){
+		assert(M->start != nullptr);
+		assert(M->rows > rows);
+		assert(M->cols > cols);
+		
+		M->start[rows*M->cols + cols] = item;
 	}
 }
-
-dataT MATRIX_GET_ITEM_AT(Matrix* M, int rows, int cols){
-	return M->start[rows*M->cols + cols];
-}
-
-void MATRIX_SET_ITEM_AT(Matrix* M, dataT item, int rows, int cols){
-	assert(M->start != nullptr);
-	assert(M->rows > rows);
-	assert(M->cols > cols);
-	
-	M->start[rows*M->cols + cols] = item;
-}
-
 #endif // NEURAL_IMPLEMENTATIONS
